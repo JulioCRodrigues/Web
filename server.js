6110be8db02c2593e67cabacb50d6bcbb39c161e
Julio Rodrigues <33026055+D4RKL4N@users.noreply.github.com>
2020-02-20 10:32:57 -0300

    First commit

---
+++ server.js
@@ -0,0 +1,82 @@
+//configurando o servidor
+const express = require("express")
+const server = express()
+
+
+
+
+//habilitar body do formulario
+server.use(express.urlencoded({extended: true}))
+
+
+//configurar a apresentação da página
+server.get("/", function(req, res){
+
+    db.query("select * from donors", function(err, result){
+        if (err) return res.send("Existe um erro no banco de dados!")
+
+        const donors = result.rows
+
+        return res.render("index.html", { donors})
+    })
+})
+
+
+server.post("/", function(req, res){
+
+    //pegar dados do formulario
+    const name = req.body.name
+    const email = req.body.email
+    const blood = req.body.blood
+
+    //restrição para que nenhum campo fique vazio
+    if (name == "" || email == "" || blood == ""){
+        return res.send("Todos os campos são obrigatórios!")
+    }
+
+    // adicionar o valor dentro dobanco de dados
+
+    const query = `INSERT INTO donors ("name", "email", "blood") VALUES ($1,$2,$3)`
+    const values = [name, email, blood]
+
+    db.query(query, values, function(err){
+
+        //fluxo de erro
+        if (err) return res.send("Erro no banco de dados!")
+
+        //fluxo ideal
+        return res.redirect("/")
+    })
+
+    
+})
+
+
+//configurar o servidor para apresentar arquivos estaticos
+server.use(express.static('public'))
+
+//configurar a conexão com o banco e dados
+const Pool = require('pg').Pool
+const db = new Pool({
+    user: 'postgres',
+    password: '0000',
+    host: 'localhost',
+    port: 5432,
+    database: 'doe'
+
+})
+
+
+//configurando a tamplate engine
+const nunjucks = require("nunjucks")
+nunjucks.configure("./", {
+    express: server,
+    noCache: true, //boolean aceita 2 valores V OU F 
+})
+
+
+//ligar o servidor e permitir o acesso na porta 3000
+
+server.listen(3000, function(){
+    console.log("iniciei o servidor")
+})
